c9f76f5abfff9c9c0d0d08a7adb80d73126e2966
jupiterio <71483657+jupiterio@users.noreply.github.com>
2020-08-16 14:21:35 -0600

    initial commit

---
+++ src/main/java/io/github/jupiterio/beaconator/mixin/MixinBeaconBlockEntity.java
@@ -0,0 +1,14 @@
+package io.github.jupiterio.beaconator.mixin;
+
+import net.minecraft.block.entity.BeaconBlockEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(BeaconBlockEntity.class)
+public class MixinBeaconBlockEntity {
+    @ModifyVariable(method = "applyPlayerEffects", at = @At(value = "STORE", ordinal = 0), ordinal = 0)
+    private double increaseDistance(double d) {
+        return d * 2;
+    }
+}
